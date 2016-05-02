777e96d6506952c703d598e7908affee986395ba
MacMasterBlaster <mac.orchard@yahoo.com>
2016-05-02 17:01:01 -0500

    adds item dropper

---
+++ Assets/The Adventures of a Gelatinous Hero/Scripts/ItemControllers/ItemDropPrefabController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class ItemDropPrefabController : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+    public float frameInterval = .2f;
+    private Sprite[] currentSprites;
+    private SpriteRenderer _spriteRenderer;
+
+    public SpriteRenderer spriteRenderer
+    {
+        get
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            return _spriteRenderer;
+        }
+    }
+
+    void Awake()
+    {
+        while (enabled)
+        {
+            StartCoroutine("Animate");
+        }
+    }
+
+    IEnumerator Animate()
+    {
+        for (int i = 0; i <= animationSprites.Length; i++)
+        {
+            spriteRenderer.sprite = currentSprites[i];
+        }
+        yield return new WaitForSeconds(frameInterval);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            Debug.Log("You Got Me");
+        }
+    }
+}
