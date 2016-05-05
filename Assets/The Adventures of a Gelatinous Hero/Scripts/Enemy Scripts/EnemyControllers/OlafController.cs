8035059a9e2c345772349d28e5e87befb57d9bcb
MacMasterBlaster <mac.orchard@yahoo.com>
2016-05-05 12:14:12 -0500

    modified olafPrefab and script

--- Assets/The Adventures of a Gelatinous Hero/Scripts/Enemy Scripts/EnemyControllers/OlafController.cs
+++ Assets/The Adventures of a Gelatinous Hero/Scripts/Enemy Scripts/EnemyControllers/OlafController.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class OlafController : MonoBehaviour
 {
 
-    public float speed = 1;
+    public float walkSpeed = 1;
+    public float jumpSpeed = 3;
     public float attackInterval = .3f;
+    public float walkTime = 5;
+    public float jumpTime = 1;
     private Rigidbody2D _body;
     private Animator animator;
 
@@ -21,17 +24,39 @@ public class OlafController : MonoBehaviour
     {
         _body = gameObject.GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
+        //StartCoroutine("Walk");
+        Charge();
+    }
+
+    void Charge()
+    {
+        StartCoroutine("Walk");
+        ThrustAttack();
+        StartCoroutine("Walk");
+    }
+
+    void ThrustAttack()
+    {
+        StartCoroutine("Thrust");
     }
 
     IEnumerator Thrust()
     {
         animator.SetTrigger("Thrust");
+        StopCoroutine("Walk");
+        //body.velocity = Vector2.zero;
         yield return new WaitForSeconds(attackInterval);
     }
 
     IEnumerator Walk()
     {
+        body.velocity = Vector2.down * walkSpeed;
+        yield return new WaitForSeconds(walkTime);
+    }
 
-        yield return new WaitForSeconds(attackInterval);
+    IEnumerator Jump()
+    {
+        body.velocity = Vector2.up * jumpSpeed;
+        yield return new WaitForSeconds(jumpTime);
     }
 }
