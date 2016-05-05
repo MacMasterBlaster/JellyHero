de8dba555513d00cf76d5a7a02f2da279063aec8
MacMasterBlaster <mac.orchard@yahoo.com>
2016-05-04 21:46:40 -0500

    Updates OlafController script

--- Assets/The Adventures of a Gelatinous Hero/Scripts/Enemy Scripts/EnemyControllers/OlafController.cs
+++ Assets/The Adventures of a Gelatinous Hero/Scripts/Enemy Scripts/EnemyControllers/OlafController.cs
@@ -1,7 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OlafController : MonoBehaviour {
+public class OlafController : MonoBehaviour
+{
 
+    public float speed = 1;
+    public float attackFrameInterval = .3f;
+    private Rigidbody2D _body;
+    private Animator animator;
 
+    public Rigidbody2D body
+    {
+        get
+        {
+            return _body;
+        }
+    }
+
+    void Awake()
+    {
+        _body = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    IEnumerator Thrust()
+    {
+        animator.Play("Thrust");
+        yield return new WaitForSeconds(attackFrameInterval);
+    }
+
+    IEnumerator Walk()
+    {
+        yield return new WaitForSeconds(attackFrameInterval);
+    }
 }
